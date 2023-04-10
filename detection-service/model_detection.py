a42b9b13775a402cdba1878eb653a0c3cd4e8f62
Cem Anaral <cemanaral@hotmail.com>
2023-04-10 23:52:27 +0300

    refactor: use TfDetectionWrapper in brand_detection

--- detection-service/model_detection.py
+++ detection-service/model_detection.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, current_app, jsonify
 import keras
-import numpy as np
-import cv2
+
+from tf_detection_wrapper import TfDetectionWrapper
+
 
 blueprint = Blueprint('model_detection', __name__)
 
@@ -14,26 +15,7 @@ def ford_model_detection():
     current_app.logger.info(
         f'file received from ford model detection endpoint {model_image.__dict__}')
     
-    filestr = request.files['model_image'].read()
-    file_bytes = np.fromstring(filestr, np.uint8)
+    detector = TfDetectionWrapper(ford_model_detection_model, request.files['model_image'].read(), ford_models)
+    return detector.detection_response()
 
-    im = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im = cv2.resize(im, dsize=(200, 300), interpolation=cv2.INTER_AREA)
-
-    X = []
-    X.append(im)
-    X = np.array(X)
-    X = X.astype('float32')
-    X = X /255.0
-    X = X.reshape(-1,300,200,1)
-
-    prediction_result = ford_model_detection_model.predict(X)
-    
-    print(ford_models)
-    print(prediction_result[0])
-    
-    prediction_result_list = prediction_result[0].tolist()
-    prediction_result_index = prediction_result_list.index(max(prediction_result_list)) 
-    return jsonify(result=ford_models[prediction_result_index])
     
