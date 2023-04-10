1d19a010e6b93c4a04d5460c952f4d49e61a071d
Cem Anaral <cemanaral@hotmail.com>
2023-04-10 23:48:03 +0300

    feat: implement TfDetectionWrapper

--- detection-service/brand_detection.py
+++ detection-service/brand_detection.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, current_app, jsonify
 import keras
-import numpy as np
-import cv2
+
+from tf_detection_wrapper import TfDetectionWrapper
 
 blueprint = Blueprint('brand_detection', __name__)
 
@@ -15,26 +15,5 @@ def brand_detection():
     current_app.logger.info(
         f'file received from brand endpoint {model_image.__dict__}')
     
-    filestr = request.files['model_image'].read()
-    file_bytes = np.fromstring(filestr, np.uint8)
-
-    im = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im = cv2.resize(im, dsize=(200, 300), interpolation=cv2.INTER_AREA)
-
-    X = []
-    X.append(im)
-    X = np.array(X)
-    X = X.astype('float32')
-    X = X /255.0
-    X = X.reshape(-1,300,200,1)
-
-    prediction_result = brand_detection_model.predict(X)
-    
-    print(car_brands)
-    print(prediction_result[0])
-    
-    prediction_result_list = prediction_result[0].tolist()
-    prediction_result_index = prediction_result_list.index(max(prediction_result_list)) 
-    return jsonify(result=car_brands[prediction_result_index])
-    
+    detector = TfDetectionWrapper(brand_detection_model, request.files['model_image'].read(), car_brands)
+    return detector.detection_response()
