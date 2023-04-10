e3913897b09d0180a84a9f3beece66535ae6cae2
Cem Anaral <cemanaral@hotmail.com>
2023-04-10 23:36:32 +0300

    feat: add cors

--- detection-service/app.py
+++ detection-service/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, json
+from flask_cors import CORS
+
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
+CORS(app)
 
 from model_detection import blueprint as model_detection_blueprint
 from brand_detection import blueprint as brand_detection_blueprint
