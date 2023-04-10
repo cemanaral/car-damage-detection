282c8c532af600ad03ce9243dff254dae5b1bcd9
yhasansenyurt <yhsenyurt@gmail.com>
2023-04-10 15:00:04 +0300

    web API added.

---
+++ damagewiz-web/src/main/java/net/damagewiz/damagewizweb/DamagewizWebApplication.java
@@ -0,0 +1,13 @@
+package net.damagewiz.damagewizweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamagewizWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamagewizWebApplication.class, args);
+	}
+
+}
---
+++ damagewiz-web/src/test/java/net/damagewiz/damagewizweb/DamagewizWebApplicationTests.java
@@ -0,0 +1,13 @@
+package net.damagewiz.damagewizweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamagewizWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
