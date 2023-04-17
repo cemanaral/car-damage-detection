30b85cd3a89424995bd42c5bfdcc1e47043674bc
Cem Anaral <cemanaral@hotmail.com>
2023-04-17 23:10:26 +0300

    fix: convert im to numpy array

--- damaged-part-detection-service/damaged_part_detection.py
+++ damaged-part-detection-service/damaged_part_detection.py
@@ -139,7 +139,7 @@ parts_class_map = {0: 'headlamp', 1: 'rear_bumper',
 
 @app.route('/', methods=['POST'])
 def detect_parts():
-    im = np.as_array(request.files['model_image'].read())
+    im = np.asarray(request.files['model_image'].read())
     app.logger.info(f'file received from brand endpoint {request.files["model_image"].__dict__}')
 
 
