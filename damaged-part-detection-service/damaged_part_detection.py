6bbf8ebc707704c5c6ec4a2769ede2613737e34f
Cem Anaral <cemanaral@hotmail.com>
2023-04-17 23:01:57 +0300

    change device to cpu from gpu

--- damaged-part-detection-service/damaged_part_detection.py
+++ damaged-part-detection-service/damaged_part_detection.py
@@ -75,7 +75,7 @@ cfg.MODEL.RETINANET.NUM_CLASSES = 2  # only has one class (damage) + 1
 cfg.MODEL.WEIGHTS = os.path.join(
     "models/damage_segmentation_model.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
-cfg['MODEL']['DEVICE'] = 'cuda'  # or cpu
+cfg['MODEL']['DEVICE'] = 'cpu'  # or cpu
 damage_predictor = DefaultPredictor(cfg)
 
 
